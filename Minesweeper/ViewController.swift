fa94666ba31dcd700c8258930c74a28fb8125f62
Soo-Han Lee <martino_lee@naver.com>
2019-06-09 20:59:42 +0800

    Initial Commit

---
+++ Minesweeper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minesweeper
+//
+//  Created by 이수한 on 09/06/2019.
+//  Copyright © 2019 Soohan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
